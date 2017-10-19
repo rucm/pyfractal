de94f0ec1ecc9839948a0a5655abc0ae479a03f5
cm ru <cm ru>
2017-10-19 22:57:24 +0900

    Maintenance of fractal.py.

--- fractal.py
+++ fractal.py
@@ -9,33 +9,33 @@ from PIL import Image
 
 
 @jit('Tuple((i8,f8))(c16, i8)', nopython=True)
-def mandelbrot(c, repeat_count):
-    real, imag, nreal, zabs = 0.0, 0.0, 0.0, 0.0
-    for n in range(repeat_count):
+def mandelbrot(c, repeat_cnt):
+    real, imag, nreal, z_abs = 0.0, 0.0, 0.0, 0.0
+    for n in range(repeat_cnt):
         nreal = real * real - imag * imag + c.real
         imag = 2 * real * imag + c.imag
         real = nreal
-        zabs = real * real + imag * imag
-        if zabs > 2.0:
-            return n, zabs
-    return 0, zabs
+        z_abs = real * real + imag * imag
+        if z_abs > 2.0:
+            return n, z_abs
+    return 0, z_abs
 
 
 @guvectorize(
     ['(c16[:],i8[:],f8[:],f8[:])'],
     '(n),()->(n),(n)',
     target='parallel')
-def mandelbrot_numpy(c, repeat_counts, out1, out2):
-    repeat_count = repeat_counts[0]
+def mandelbrot_numpy(c, repeat_cnts, out1, out2):
+    repeat_cnt = repeat_cnts[0]
     for i in range(c.shape[0]):
-        out1[i], out2[i] = mandelbrot(c[i], repeat_count)
+        out1[i], out2[i] = mandelbrot(c[i], repeat_cnt)
 
 
-def mandelbrot_set(xmin, xmax, ymin, ymax, cx, cy, width, height, repeat_count):
+def mandelbrot_set(xmin, xmax, ymin, ymax, width, height, repeat_cnt):
     r1 = np.linspace(xmin, xmax, width, dtype='f8')
     r2 = np.linspace(ymin, ymax, height, dtype='f8')
     c = r1 + r2[:, None] * 1j
-    n3, n4 = mandelbrot_numpy(c, repeat_count)
+    n3, n4 = mandelbrot_numpy(c, repeat_cnt)
     return r1, r2, n3.T, n4.T
 
 
@@ -45,15 +45,15 @@ def mandelbrot_set(xmin, xmax, ymin, ymax, cx, cy, width, height, repeat_count):
 
 
 @jit('Tuple((i8,f8))(c16, c16, i8)', nopython=True)
-def julia(z, c, repeat_count):
-    real, imag, nreal, zabs = z.real, z.imag, 0.0, 0.0
-    for n in range(repeat_count):
+def julia(z, c, repeat_cnt):
+    real, imag, nreal, z_abs = z.real, z.imag, 0.0, 0.0
+    for n in range(repeat_cnt):
         nreal = real * real - imag * imag + c.real
         imag = 2 * real * imag + c.imag
         real = nreal
-        zabs = real * real + imag * imag
-        if zabs > 4.0:
-            return n, zabs
+        z_abs = real * real + imag * imag
+        if z_abs > 4.0:
+            return n, z_abs
     return 0, 0
 
 
@@ -61,18 +61,18 @@ def julia(z, c, repeat_count):
     ['(c16[:],c16[:],i8[:],f8[:],f8[:])'],
     '(n),(),()->(n),(n)',
     target='parallel')
-def julia_numpy(z, c, repeat_counts, out1, out2):
-    repeat_count = repeat_counts[0]
+def julia_numpy(z, c, repeat_cnts, out1, out2):
+    repeat_cnt = repeat_cnts[0]
     _c = c[0]
     for i in range(z.shape[0]):
-        out1[i], out2[i] = julia(z[i], _c, repeat_count)
+        out1[i], out2[i] = julia(z[i], _c, repeat_cnt)
 
 
-def julia_set(xmin, xmax, ymin, ymax, cx, cy, width, height, repeat_count):
+def julia_set(xmin, xmax, ymin, ymax, cx, cy, width, height, repeat_cnt):
     r1 = np.linspace(xmin, xmax, width, dtype='f8')
     r2 = np.linspace(ymin, ymax, height, dtype='f8')
     z = r1 + r2[:, None] * 1j
-    n3, n4 = julia_numpy(z, cx + cy * 1j, repeat_count)
+    n3, n4 = julia_numpy(z, cx + cy * 1j, repeat_cnt)
     return r1, r2, n3.T, n4.T
 
 
@@ -88,20 +88,79 @@ def min_max(data, M=1, m=0, axis=None):
     return (data - mn) / (mx - mn) * (M - m) + m
 
 
-def create_image(data, size, h=0, s=255, v=255):
-    img = Image.new('HSV', (size, size), (h, s, v))
+def create_image(size, h, s, v):
+    image = Image.new('HSV', (size, size), (h, s, v))
+    return image
+
+
+def save_image(image, filename, filetype):
+    img = image.convert('RGB')
+    img.save(filename, filetype)
+
+
+@guvectorize(
+    ['(i4[:,:,:],i4[:,:],i4[:],i4[:,:,:])'],
+    '(n,m,p),(n,m),()->(n,m,p)',
+    target='parallel')
+def __change_color(image, d, indices, output):
+    index = indices[0]
+    for i in range(d.shape[0]):
+        for j in range(d.shape[1]):
+            color = image[i, j]
+            color[index] = d[i, j]
+            output[i, j] = color
+
+
+def change_hue(image, data):
+    d = data.astype(np.uint8)
+    image_array = np.asarray(image)
+    image_array = __change_color(image_array, d, 0)
+    img = Image.fromarray(np.uint8(image_array), mode='HSV')
+    return img
+
+
+def change_saturation(image, data):
+    d = data.astype(np.uint8)
+    image_array = np.asarray(image)
+    image_array = __change_color(image_array, d, 1)
+    img = Image.fromarray(np.uint8(image_array), mode='HSV')
+    return img
+
+
+def change_value(image, data):
+    d = data.astype(np.uint8)
+    image_array = np.asarray(image)
+    image_array = __change_color(image_array, d, 2)
+    img = Image.fromarray(np.uint8(image_array), mode='HSV')
+    return img
+
+
+def to_qpixmap(image):
+    from PIL import ImageQt
+    img = image.convert('RGB')
+    return ImageQt.toqpixmap(img)
 
 
 if __name__ == '__main__':
-    from timeit import timeit
-    preset = [
-        '-1.5, 1.5, -1.5, 1.5, -0.3, -0.63, 400, 400, 256',
-        '-1.5, 1.5, -1.5, 1.5, -0.3, -0.63, 400, 400, 2048',
-        '-1.5, 1.5, -1.5, 1.5, -0.3, -0.63, 800, 800, 256',
-        '-1.5, 1.5, -1.5, 1.5, -0.3, -0.63, 800, 800, 2048',
-        '-1.5, 1.5, -1.5, 1.5, -0.3, -0.63, 2048, 2048, 256',
-        '-1.5, 1.5, -1.5, 1.5, -0.3, -0.63, 2048, 2048, 2048'
-    ]
-    for i, s in enumerate(preset):
-        t = timeit('julia_set({})'.format(s), globals=globals(), number=1)
-        print('preset {}: {:.3f}s'.format(i + 1, t))
+    # from timeit import timeit
+    # preset = [
+    #     '-1.5, 1.5, -1.5, 1.5, -0.3, -0.63, 400, 400, 256',
+    #     '-1.5, 1.5, -1.5, 1.5, -0.3, -0.63, 400, 400, 2048',
+    #     '-1.5, 1.5, -1.5, 1.5, -0.3, -0.63, 800, 800, 256',
+    #     '-1.5, 1.5, -1.5, 1.5, -0.3, -0.63, 800, 800, 2048',
+    #     '-1.5, 1.5, -1.5, 1.5, -0.3, -0.63, 2048, 2048, 256',
+    #     '-1.5, 1.5, -1.5, 1.5, -0.3, -0.63, 2048, 2048, 2048'
+    # ]
+    # for i, s in enumerate(preset):
+    #     t = timeit('julia_set({})'.format(s), globals=globals(), number=1)
+    #     print('preset {}: {:.3f}s'.format(i + 1, t))
+
+    _, _, n, _ = julia_set(
+        -1.5, 1.5, -1.5, 1.5, -0.3, -0.63, 400, 400, 256
+    )
+
+    n = min_max(n, 255, 0).T
+
+    image = create_image(400, 128, 255, 255)
+    image = change_value(image, n)
+    save_image(image, 'test.png', 'PNG')
--- models.py
+++ models.py
@@ -13,17 +13,24 @@ class FractalData(QObject):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.set_default_data()
+        self.image = None
+        self.div = None
+        self.z_abs = None
+        self.item = QGraphicsPixmapItem()
 
     def set_default_data(self):
-        self.x, self.y = 0.0, 0.0
+        self.use_h = 'default'
+        self.use_s = 'default'
+        self.use_v = 'div'
+        self.h, self.s, self.v = 0.5, 1.0, 1.0
         self.cx, self.cy = 0.0, 0.0
-        self.repeat_count = 256
+        self.repeat_cnt = 256
         self.scale = 1.0
         self.size = 800
         self.xmin, self.xmax = -1.5, 1.5
         self.ymin, self.ymax = -1.5, 1.5
 
-    def set_range(self, xmin, xmax, ymin, ymax):
+    def set_data(self, xmin, xmax, ymin, ymax):
         self.xmin, self.xmax = xmin, xmax
         self.ymin, self.ymax = ymin, ymax
 
@@ -33,11 +40,45 @@ class FractalData(QObject):
     def set_scale(self, scale):
         self.scale = scale
 
-    def set_repeat_count(self, repeat_count):
-        self.repeat_count = repeat_count
+    def set_repeat_cnt(self, repeat_cnt):
+        self.repeat_cnt = repeat_cnt
 
     def set_constant(self, cx, cy):
         self.cx, self.cy = cx, cy
+
+    def set_hsv(self, h, s, v):
+        self.h, self.s, self.v = h, s, v
+
+    # param list: default, div, z-abs
+    def set_use_param(self, use_h='default', use_s='default', use_v='default'):
+        self.use_h = use_h
+        self.use_s = use_s
+        self.use_v = use_v
+
+    def calculate(self):
+        import fractal
+        _, _, self.div, self.z_abs = fractal.mandelbrot_set(
+            self.xmin, self.xmax, self.ymin, self.ymax,
+            self.size, self.size,
+            self.repeat_cnt
+        )
+        self.div = fractal.min_max(self.div, 255, 0)
+        self.z_abs = fractal.min_max(self.z_abs, 255, 0)
+        self.image = fractal.create_image(self.size, self.h, self.s, self.v)
+        if self.use_h == 'div':
+            self.image = fractal.change_hue(self.image, self.div)
+        elif self.use_h == 'z_abs':
+            self.image = fractal.change_hue(self.image, self.z_abs)
+        if self.use_s == 'div':
+            self.image = fractal.change_saturation(self.image, self.div)
+        elif self.use_s == 'z_abs':
+            self.image = fractal.change_saturation(self.image, self.z_abs)
+        if self.use_v == 'div':
+            self.image = fractal.change_value(self.image, self.div)
+        elif self.use_v == 'z_abs':
+            self.image = fractal.change_value(self.image, self.z_abs)
+        pixmap = fractal.to_qpixmap(self.image)
+        self.item.setPixmap(pixmap)
 
 
 class JuliaData(QObject):
@@ -46,11 +87,11 @@ class JuliaData(QObject):
         super().__init__(parent)
         self.graph = QGraphicsPixmapItem()
 
-    def init_data(self, size, x, y, scale, cx, cy, repeat_count):
+    def init_data(self, size, x, y, scale, cx, cy, repeat_cnt):
         self.size = size
         self.x, self.y = x, y
         self.cx, self.cy = cx, cy
-        self.repeat_count = repeat_count
+        self.repeat_cnt = repeat_cnt
         self.scale = scale
         self.lower = -1.5 / scale
         self.upper = 1.5 / scale
@@ -65,9 +106,8 @@ class JuliaData(QObject):
         _, _, n, m = mandelbrot_set(
             self.lower + self.x, self.upper + self.x,
             self.lower + self.y, self.upper + self.y,
-            self.cx, self.cy,
             self.size, self.size,
-            self.repeat_count
+            self.repeat_cnt
         )
 
         n = min_max(n)
